58753ffc284080f266d179ca8af21c193236b372
berhane <berhane4321@gmail.com>
2020-09-12 08:31:00 -0700

    adds saved page to load the saved news from database

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Navbar } from 'react-bulma-components/dist';
 import "./App.css";
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar color="dark" >PLease work</Navbar>
+     <div className="App">
+        <Navbar color="primary" >PLease work</Navbar>
         <h1>Happily Woke!</h1>
+      </div>
       </div>
     );
   }
--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,6 +1,45 @@
-import React from "react";
-const Saved = () => {
+import React, { useState, useEffect } from "react";
+import{ Columns,Button} from "react-bulma-components/lib/components/columns";
 
-};
+
+function Saved() {
+  // Setting our component's initial state
+  const [news, setNews] = useState([]);
+
+  // Load all news and store them with setNews
+  useEffect(() => {
+    loadSavedNews();
+  }, []);
+
+  // Loads all saved news and sets them to news
+  function loadSavedNews() {
+    API.getSavedNews()
+      .then((res) => setNews(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  // Deletes news from the database with a given id, then reloads books from the db
+  function deleteNews(id) {
+    API.deleteNews(id)
+      .then((res) => loadSavedNews())
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div>
+      {news.map((item) => (
+        <Columns>
+          <Columns.Column size={4}>
+            <Card articles={item}> </Card>
+            <Button onClick={() => deleteNews(item._id)} 
+             color="success">
+              Delete
+            </Button>
+          </Columns.Column>
+        </Columns>
+      ))}
+    </div>
+  );
+}
 
 export default Saved;
