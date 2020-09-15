1f028038d7d0642bdcda4140654beb87a3030dec
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-09-14 21:59:12 -0700

    Adds moment.js date conversion and Save/Remove Button to Cards. Adds card turnary for card with no image. Adds spinner for card loading time.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,33 +1,74 @@
 import React from 'react'
-// import ArticleMood from '../ArticleMood';
 import './style.css'
+import { useLocation } from 'react-router-dom';
+import SaveFooter from '../SaveFooter';
+import DeleteFooter from '../DeleteFooter';
 
-const Card = ({ article }) => {
-  console.log(article)
+export const CardYesImage = ({ article }) => {
+  let location = useLocation();
+
+  //convert time
+  const moment = require('moment-timezone');
+  let utcTime = article.publishedAt;
+  const local_date = moment.tz(utcTime, "America/New_York").format('MMM Do YYYY h:mmA');
+  console.log("local_date", local_date);
+
   return (
-    <div className="card">
+    <div
+      className="card equal-height">
       <div className="card-image">
-        <a href={article.url}>
-        <figure className="image is-3by2">
-          <img
-          src={article.urlToImage} alt={article.title}>
-          </img>
-        </figure>
+        <a href={article.url} target="_blank" rel="noopener noreferrer">
+          <figure className="image is-3by2">
+            <img
+              src={article.urlToImage} alt={article.title}>
+            </img>
+          </figure>
         </a>
       </div>
       <div className="card-content">
         <a href={article.url}
-        className="title is-4 art-title">{article.title} </a>
-        
-        <p>Published: {article.publishedAt}</p>
+          className="title is-4 art-title" target="_blank" rel="noopener noreferrer">{article.title}</a>
+
+        <p>Published on {local_date} EST by {article.source.name}</p>
+
         <br>
         </br>
-        
+
         <p>{article.description}</p>
+
       </div>
-      {/* <ArticleMood/> */}
+      {location.pathname === "/" ? <SaveFooter article={article} /> : <DeleteFooter />}
     </div>
   )
 };
 
-export default Card
+export const CardNoImage = ({ article }) => {
+  let location = useLocation();
+
+  //convert date and time
+  const moment = require('moment-timezone');
+  let utcTime = article.publishedAt;
+  const local_date = moment.tz(utcTime, "America/New_York").format('MMM Do YYYY h:mmA');
+  console.log("local_date", local_date);
+
+  return (
+    <div className="card equal-height">
+
+      <div className="card-content center-no-image-par">
+        <div className="center-no-image-ch">
+          <a href={article.url} target="_blank" rel="noopener noreferrer"
+            className="title is-4 art-title">{article.title} </a>
+
+          <p>Published on {local_date} EST by {article.source.name}</p>
+          <br>
+          </br>
+
+          <p>{article.description}</p>
+
+        </div>
+      </div>
+
+      {location.pathname === "/" ? <SaveFooter article={article} /> : <DeleteFooter />}
+    </div>
+  )
+};
---
+++ client/src/components/DeleteFooter/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function DeleteFooter() {
+  return (
+  
+    <footer class="card-footer">
+        {/* <button
+          className="card-footer-item button">Mood Check-in</button> */}
+        <button className="card-footer-item button">Remove</button>
+      </footer>
+    
+  )
+}
+
+export default DeleteFooter
