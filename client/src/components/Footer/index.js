a9cd9ebb30661ee95312ede29afbe72ab8e20ec5
berhane <berhane4321@gmail.com>
2020-09-19 12:45:57 -0700

    add footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="footer foot">
       <div className="content foot-content">
-        <p>&copy; 2020 HappilyWoke.com</p>
+        <p>&copy; 2020 happilyWoke.com</p>
       </div>
     </div>
   );
