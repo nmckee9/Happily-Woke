9ac9ca59084a121ee794193414e8ef794888f6e1
maquispe <62685159+maquispe@users.noreply.github.com>
2020-09-22 21:08:58 -0700

    fixes .env

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router()
 const News = require("../models")
+const axios = require("axios");
+require('dotenv').config();
+
+router.get("/api/getnews", (req, res) => {
+    console.log("got here")
+     axios.get('https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=' + process.env.REACT_APP_API_KEY).then(data => {
+         console.log("data.data", data.data)
+        res.json(data.data)
+   })
+})
+router.get("/api/getsearchednews/:query", (req, res) => {
+    console.log("req.params.query)", req.params.query)
+    axios.get('https://newsapi.org/v2/everything?q=' + req.params.query +  '&pageSize=100&apiKey=' + process.env.REACT_APP_API_KEY).then(data => {
+        res.json(data.data)
+})
+})
 
 router.get("/api/articles", function(req, res){
     console.log("GET ROUTE")
